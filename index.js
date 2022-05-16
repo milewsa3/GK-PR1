51a25562b4a1e01cc76f7dc6fab73e5754ebc5a0
Adrian <a.milewski@onet.eu>
2022-05-16 15:09:13 +0200

    Working version :)

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import Cuboid from "./Cuboid.js";
 // let paper1 = new Cuboid(-10, -20, 105, 1, 90, "red");
 let paper2 = new Cuboid(-40, -40, 90, 90, 90, 5, "yellow");
 let paper3 = new Cuboid(10, -20, 105, 90, 90, 5, "blue");
-let scene = new Scene("canvas", [paper2, paper3]);
+let paper1 = new Cuboid(10, -80, 120, 90, 90, 5, "red");
+let scene = new Scene("canvas", [paper2, paper3, paper1]);
 let controller = new Controller(scene);
 
 controller.run();
